26036221f0270ba0158686961c407f5c8ade8f50
Vlad Frolov <frolvlad@gmail.com>
2020-10-18 23:54:51 +0300

    use autobahn instead of wampy to be able to connect to wamp from node.js

--- explorer-api.js
+++ explorer-api.js
@@ -1,25 +1,50 @@
-const { Wampy } = require('wampy');
+const autobahn = require("autobahn");
 
-const WAMP_NEAR_EXPLORER_URL = process.env.WAMP_NEAR_EXPLORER_URL || 'wss://near-explorer-wamp.onrender.com/ws'
-const WAMP_NEAR_EXPLORER_TOPIC_PREFIX = process.env.WAMP_NEAR_EXPLORER_TOPIC_PREFIX || 'com.nearprotocol.mainnet.explorer'
-
-const wamp = new Wampy(WAMP_NEAR_EXPLORER_URL, { realm: 'near-explorer' })
-
-const queryExplorer = (sql, params) => new Promise((resolve, reject) => wamp.call(
-    `${WAMP_NEAR_EXPLORER_TOPIC_PREFIX}.select`,
-    [sql, params],
-    {
-        onSuccess(dataArr) {
-            resolve(dataArr[0]);
+exports.WampNearExplorerConnection = class {
+  constructor(wampNearExplorerUrl) {
+    this.connection = new autobahn.Connection({
+      realm: "near-explorer",
+      transports: [
+        {
+          url: wampNearExplorerUrl,
+          type: "websocket",
         },
-        onError(...args) {
-            console.log(args)
-            reject(args);
-        }
-    }
-));
+      ],
+      retry_if_unreachable: true,
+      max_retries: Number.MAX_SAFE_INTEGER,
+      max_retry_delay: 10,
+    });
+  }
 
-async function getLockups() {
+  open() {
+    return new Promise((resolve, reject) => {
+      this.connection.onopen = (session) => resolve(session);
+      this.connection.onclose = (reason) => reject(reason);
+      this.connection.open();
+    });
+  }
+
+  close() {
+    this.connection._transport.onclose = () => {};
+    this.connection.close();
+  }
+};
+
+exports.ExplorerApi = class {
+  constructor(wampNearExplorerSession, wampNearExplorerTopicPrefix) {
+    this.wampNearExplorerSession = wampNearExplorerSession;
+    this.wampNearExplorerTopicPrefix = wampNearExplorerTopicPrefix;
+  }
+
+  async call(handlerName, args, kwargs) {
+    return await this.wampNearExplorerSession.call(
+      `${this.wampNearExplorerTopicPrefix}.${handlerName}`,
+      args,
+      kwargs
+    );
+  }
+
+  async getLockups(wampConnection) {
     const sql = `
         SELECT 
             DISTINCT receiver_id
@@ -27,18 +52,18 @@ async function getLockups() {
             transactions
         WHERE
             receiver_id LIKE '%.lockup.near'
+    `;
+
+    /*
         LIMIT 
             :offset, :count
-    `
-
     const params = {
-        offset: 0,
-        count: 5
-    }
+      offset: 0,
+      count: 5,
+    };
+    */
 
-    return queryExplorer(sql, params)
-}
-
-module.exports = {
-    getLockups,
+    const uniqueLockupAccountIds = await this.call("select", [sql]);
+    return uniqueLockupAccountIds.map((it) => it.receiver_id);
+  }
 };
--- scripts/lockups.js
+++ scripts/lockups.js
@@ -1,8 +1,128 @@
-const { getLockups } = require('../explorer-api');
+const BN = require("bn.js");
+const nearAPI = require("near-api-js");
+
+const { WampNearExplorerConnection, ExplorerApi } = require("../explorer-api");
+
+const WAMP_NEAR_EXPLORER_URL =
+  process.env.WAMP_NEAR_EXPLORER_URL ||
+  "wss://near-explorer-wamp.onrender.com/ws";
+const WAMP_NEAR_EXPLORER_TOPIC_PREFIX =
+  process.env.WAMP_NEAR_EXPLORER_TOPIC_PREFIX ||
+  "com.nearprotocol.mainnet.explorer";
+const NEAR_RPC_URL = process.env.NEAR_RPC_URL || "https://rpc.mainnet.near.org";
+
+function readOption(reader, f) {
+  let x = reader.read_u8();
+  if (x == 1) {
+    return f();
+  }
+  return null;
+}
+
+function decodeLockupState(lockupContractState) {
+  let reader = new nearAPI.utils.serialize.BinaryReader(lockupContractState);
+  let owner = reader.read_string();
+  let lockupAmount = reader.read_u128().toString();
+  let terminationWithdrawnTokens = reader.read_u128().toString();
+  let lockupDuration = reader.read_u64().toString();
+  let releaseDuration = readOption(reader, () => reader.read_u64().toString());
+  let lockupTimestamp = readOption(reader, () => reader.read_u64().toString());
+  let tiType = reader.read_u8();
+  let transferInformation;
+  if (tiType == 0) {
+    transferInformation = {
+      transfers_timestamp: reader.read_u64(),
+    };
+  } else {
+    transferInformation = {
+      transfer_poll_account_id: reader.read_string(),
+    };
+  }
+  let vestingType = reader.read_u8();
+  vestingInformation = null;
+  if (vestingType == 1) {
+    vestingInformation = {
+      VestingHash: reader.read_array(() => reader.read_u8()),
+    };
+  } else if (vestingType == 2) {
+    let vestingStart = reader.read_u64();
+    let vestingCliff = reader.read_u64();
+    let vestingEnd = reader.read_u64();
+    vestingInformation = { vestingStart, vestingCliff, vestingEnd };
+  } else if (vestingType == 3) {
+    vestingInformation = "TODO";
+  }
+  return {
+    owner,
+    lockupAmount,
+    terminationWithdrawnTokens,
+    lockupDuration,
+    releaseDuration,
+    lockupTimestamp,
+    transferInformation,
+    vestingInformation,
+  };
+}
 
 (async () => {
-    console.log("Lockup analytics");
+  console.log("Lockup analytics");
+  const explorerConnection = new WampNearExplorerConnection(
+    WAMP_NEAR_EXPLORER_URL
+  );
+  const explorerApi = new ExplorerApi(
+    await explorerConnection.open(),
+    WAMP_NEAR_EXPLORER_TOPIC_PREFIX
+  );
 
-    let lockups = await getLockups();
-    console.log(lockups);
+  const nearRpc = new nearAPI.providers.JsonRpcProvider(NEAR_RPC_URL);
+
+  // TODO: Provide an equivalent method in near-api-js, so we don't need to hack it around.
+  nearRpc.callViewMethod = async function (contractName, methodName, args) {
+    const account = new nearAPI.Account({ provider: this });
+    return await account.viewFunction(contractName, methodName, args);
+  };
+
+  const lockupAccountIds = await explorerApi.getLockups();
+  console.log(
+    "lockup-account-id,lockup-owner-account-id,is-unlocked,lockup-balance,owner-balance"
+  );
+  for (const lockupAccountId of lockupAccountIds) {
+    const lockupOwnerAccountId = await nearRpc.callViewMethod(
+      lockupAccountId,
+      "get_owner_account_id",
+      {}
+    );
+
+    const lockupStateResponse = await nearRpc.sendJsonRpc("query", {
+      request_type: "view_state",
+      finality: "final",
+      account_id: lockupAccountId,
+      prefix_base64: "U1RBVEU=",
+    });
+    const lockupState = decodeLockupState(
+      Buffer.from(lockupStateResponse.values[0].value, "base64")
+    );
+    //console.log(lockupState, +new Date());
+
+    const isUnlocked =
+      lockupState.releaseDuration === "0" &&
+      lockupState.lockupTimestamp && parseInt(lockupState.lockupTimestamp.slice(0, -6)) < +new Date();
+
+    const lockupBalance = new BN(
+      await nearRpc.callViewMethod(lockupAccountId, "get_balance", {})
+    );
+
+    const lockupOwnerAccountInfo = await nearRpc.sendJsonRpc("query", {
+      request_type: "view_account",
+      finality: "final",
+      account_id: lockupOwnerAccountId,
+    });
+
+    const lockupOwnerBalance = new BN(lockupOwnerAccountInfo.amount).add(
+      new BN(lockupOwnerAccountInfo.locked)
+    );
+    console.log(
+      `${lockupAccountId},${lockupOwnerAccountId},${isUnlocked},${lockupBalance.toString()},${lockupOwnerBalance.toString()}`
+    );
+  }
 })().catch((error) => console.error(error));
